59dcbb05c66df17646a51f3654cd4991d2a0031d
Matthias Nösner <Equanox@users.noreply.github.com>
2021-10-25 23:40:37 +0200

    timing package for measurements (#86)
    * timing package for measurements

---
+++ pkg/timing/timing.go
@@ -0,0 +1,47 @@
+package timing
+
+import (
+	"fmt"
+	"time"
+)
+
+func Track(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s\n", name, elapsed)
+}
+
+type Measurement struct {
+	// name of the measurement
+	name string
+
+	// sum is the total duration elapsed yet
+	sum time.Duration
+
+	// lastStart last time Start() was called
+	lastStart time.Time
+
+	// cucles ins the number of Start()/Stop() cycles
+	cycles uint
+}
+
+func NewMeasurement(name string) *Measurement {
+	return &Measurement{
+		name:      name,
+		lastStart: time.Now(),
+	}
+}
+
+func (m *Measurement) Start() {
+	m.lastStart = time.Now()
+}
+
+func (m *Measurement) Stop() {
+	elapsed := time.Since(m.lastStart)
+
+	m.sum = m.sum + elapsed
+	m.cycles++
+}
+
+func (m *Measurement) String() string {
+	return fmt.Sprintf("%s ran %d times for a total of %s\n", m.name, m.cycles, m.sum)
+}
