2e52620f3b3d9c6bb51162d0d30fb277f91e6b5f
Matthias Nösner <Equanox@users.noreply.github.com>
2021-12-01 12:24:07 +0100

    hash target dirs (#131)

--- bobtask/target/hash.go
+++ bobtask/target/hash.go
@@ -4,24 +4,62 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
+	"io/fs"
+	"os"
 	"path/filepath"
 
+	"github.com/Benchkram/bob/pkg/file"
 	"github.com/Benchkram/bob/pkg/filehash"
 )
 
-// Hash creates a hash for the rntire target
+// Hash creates a hash for the entire target
 func (t *T) Hash() (empty string, _ error) {
 	aggregatedHashes := bytes.NewBuffer([]byte{})
 	for _, f := range t.Paths {
 		target := filepath.Join(t.dir, f)
-		h, err := filehash.Hash(target)
+
+		if !file.Exists(target) {
+			return empty, fmt.Errorf("target doe not exist %q", f)
+		}
+		fi, err := os.Stat(target)
 		if err != nil {
-			return empty, fmt.Errorf("failed to hash target %q: %w", f, err)
+			return empty, fmt.Errorf("failed to get file info %q: %w", f, err)
 		}
 
-		_, err = aggregatedHashes.Write(h)
-		if err != nil {
-			return empty, fmt.Errorf("failed to write target hash to aggregated hash %q: %w", f, err)
+		if fi.IsDir() {
+			if err := filepath.WalkDir(target, func(p string, fi fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+				if fi.IsDir() {
+					return nil
+				}
+
+				h, err := filehash.Hash(p)
+				if err != nil {
+					return fmt.Errorf("failed to hash target %q: %w", f, err)
+				}
+
+				_, err = aggregatedHashes.Write(h)
+				if err != nil {
+					return fmt.Errorf("failed to write target hash to aggregated hash %q: %w", f, err)
+				}
+
+				return nil
+			}); err != nil {
+				return empty, fmt.Errorf("failed to walk dir %q: %w", target, err)
+			}
+			// TODO: what happens on a empty dir?
+		} else {
+			h, err := filehash.Hash(target)
+			if err != nil {
+				return empty, fmt.Errorf("failed to hash target %q: %w", f, err)
+			}
+
+			_, err = aggregatedHashes.Write(h)
+			if err != nil {
+				return empty, fmt.Errorf("failed to write target hash to aggregated hash %q: %w", f, err)
+			}
 		}
 	}
 
