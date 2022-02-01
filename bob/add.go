30c84396376c2fff804702bb69636ff11e06fe24
06rajesh <abhi.crushefix@gmail.com>
2022-02-01 13:14:46 +0100

    explicit protocol support added for url file type on bob workspace add

--- bob/add.go
+++ bob/add.go
@@ -1,11 +1,21 @@
 package bob
 
 import (
-	"strings"
+	"net/url"
 
 	"github.com/Benchkram/errz"
 )
 
+// Add, adds the rawurl as a repositoroy to bob workspace. Set all url if
+// explicit url set to false.
+//
+// if explicit protocol set to true,
+//
+// set the sshurl & localurl to "" if url is a valid http,
+//
+// set the httpurl and sshurl to "" if url is a filepath
+//
+// else it presumes url type as ssh and set http url to ""
 func (b *B) Add(rawurl string, explcitprotcl bool) (err error) {
 	defer errz.Recover(&err)
 
@@ -25,11 +35,13 @@ func (b *B) Add(rawurl string, explcitprotcl bool) (err error) {
 
 	httpsstr := repo.HTTPS.String()
 	sshstr := repo.SSH.String()
+	localstr := repo.Local
 
-	// if explicit protocol set to true,
-	// set the sshurl to "" if url starts with http,
-	// else http url set to ""
 	if explcitprotcl && checkIfHttp(rawurl) {
+		sshstr = ""
+		localstr = ""
+	} else if explcitprotcl && checkIfFile(rawurl) {
+		httpsstr = ""
 		sshstr = ""
 	} else if explcitprotcl {
 		httpsstr = ""
@@ -40,7 +52,7 @@ func (b *B) Add(rawurl string, explcitprotcl bool) (err error) {
 			Name:     name,
 			HTTPSUrl: httpsstr,
 			SSHUrl:   sshstr,
-			LocalUrl: repo.Local,
+			LocalUrl: localstr,
 		},
 	)
 
@@ -51,8 +63,41 @@ func (b *B) Add(rawurl string, explcitprotcl bool) (err error) {
 }
 
 // checkIfHttp returns true if url is http,
-//
-// It is easier to detect if the url is http/s protocol.
 func checkIfHttp(rawurl string) bool {
-	return strings.HasPrefix(rawurl, "http")
+	err := isValidUrl(rawurl)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return false
+	}
+
+	return u.Scheme == "http" || u.Scheme == "https"
+}
+
+// checkIfFile returns true if url is filepath
+func checkIfFile(rawurl string) bool {
+	err := isValidUrl(rawurl)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return false
+	}
+
+	return u.Scheme == "file"
+}
+
+// isValidUrl tests a string to determine if it is a well-structured url or not.
+func isValidUrl(toTest string) error {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
