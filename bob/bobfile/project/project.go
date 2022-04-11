54d7ffe930cfe9282fcb6cd07b99267ede20eb3f
equanox <matthias@benchkram.de>
2022-04-11 11:11:11 +0200

    escape -  and add :

--- bob/bobfile/project/project.go
+++ bob/bobfile/project/project.go
@@ -3,7 +3,7 @@ package project
 import "regexp"
 
 // RestrictedProjectNameChars collects the characters allowed to represent a project.
-const RestrictedProjectNameChars = `[a-zA-Z0-9/_.-]`
+const RestrictedProjectNameChars = `[a-zA-Z0-9/_.\-:]`
 
 // RestrictedProjectNamePattern is a regular expression to validate projectnames.
 var RestrictedProjectNamePattern = regexp.MustCompile(`^` + RestrictedProjectNameChars + `+$`)
