97161366159230a0812289ed20c3502f0b384241
equanox <matthias@benchkram.de>
2022-05-12 13:09:45 +0200

    return error instead of logging

--- bob/playbook/hash.go
+++ bob/playbook/hash.go
@@ -1,16 +1,15 @@
 package playbook
 
-import (
-	"github.com/benchkram/errz"
-)
+import "fmt"
 
 // PreComputeInputHashes asynchronically
-func (pb *Playbook) PreComputeInputHashes() error {
+func (pb *Playbook) PreComputeInputHashes() (err error) {
 
 	// 3 parallel goroutines was the fastest one
 	// on a test project.
 	const max = 3
 
+	var errors []error
 	sem := make(chan int, max)
 	for _, t := range pb.Tasks {
 		sem <- 1
@@ -18,10 +17,16 @@ func (pb *Playbook) PreComputeInputHashes() error {
 		// compute input hash
 		go func(tt *Status) {
 			_, err := tt.HashIn()
-			errz.Log(err)
+			if err != nil {
+				errors = append(errors, fmt.Errorf("error computing input hash on task %s, %w", tt.Name(), err))
+			}
 			<-sem
 		}(t)
 
+	}
+
+	if len(errors) > 0 {
+		return errors[0]
 	}
 
 	return nil
