ab3dc94d4df611a4e5c93b8cbd09ad58664c365f
Tasos Papalyras <tasos@rdnt.dev>
2022-05-18 17:54:31 +0300

    update dependencies (#75)
    * update modules
    * update dependencies

--- bob/bob.go
+++ bob/bob.go
@@ -153,7 +153,7 @@ func (b *B) read() (err error) {
 	}
 
 	bin, err := ioutil.ReadFile(b.WorkspaceFilePath())
-	errz.Fatal(err, "Failed to read config file")
+	errz.Fatal(err)
 
 	err = yaml.Unmarshal(bin, b)
 	if err != nil {
--- pkg/composectl/composectl.go
+++ pkg/composectl/composectl.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/benchkram/errz"
 	"io"
 	"os"
+
+	"github.com/benchkram/errz"
 
 	"github.com/compose-spec/compose-go/types"
 	"github.com/docker/cli/cli/command"
@@ -27,7 +28,7 @@ type ComposeController struct {
 	stderr pipe
 	stdin  pipe
 
-	logger  *logger
+	logger *logger
 
 	running bool
 }
@@ -120,7 +121,7 @@ func (ctl *ComposeController) Up(ctx context.Context) error {
 			Follow:     true,
 			Timestamps: false,
 		})
-		if err != nil && !errors.Is(err, context.Canceled)  {
+		if err != nil && !errors.Is(err, context.Canceled) {
 			errz.Log(err)
 		}
 	}()
