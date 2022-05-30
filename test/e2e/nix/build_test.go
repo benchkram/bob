589a32ac3d659ecd07782c89acd8e87213fab806
Andrei Boar <andrey.boar@gmail.com>
2022-05-30 15:56:17 +0300

    add custom file to cache

--- test/e2e/nix/build_test.go
+++ test/e2e/nix/build_test.go
@@ -10,7 +10,18 @@ import (
 
 	"github.com/benchkram/bob/bob"
 	"github.com/benchkram/bob/pkg/filepathutil"
+	nix2 "github.com/benchkram/bob/pkg/nix"
 )
+
+var nixCacheContent = `49ce2a3aa83ef14a:/nix/store/fcd0m68c331j7nkdxvnnpb8ggwsaiqac-bash-5.1-p16
+bf5f35bfd69b9857:/nix/store/hgl0ydlkgs6y6hx9h7k209shw3v7z77j-coreutils-9.0
+a002a12b4adc0bd2:/nix/store/1442kn5q9ah0bhhqm99f8nr76diczqgm-gnused-4.8
+a2a616a223a33f3d:/nix/store/c7062r0rh84w3v77pqwdcggrsdlvy1df-findutils-4.9.0
+5710bc1834e1cda5:/nix/store/sh0x9kihzkdz15x18ldg989pf29m4nm7-go-1.17.9
+2ad93148f49750ec:/nix/store/6jpdfshhyqic7a85j02scrbwcxh2lfic-git-2.35.3
+2954a404deb42501:/nix/store/crzgdqkbiaz30z709j8xbal7ylm29j42-go-1.18.1
+e33686c0c937b507:/nix/store/h59dfk7dwrn7d2csykh9z9xm2miqmrnz-hello-2.12
+`
 
 var _ = Describe("Testing new nix implementation", func() {
 	AfterEach(func() {
@@ -40,7 +51,12 @@ var _ = Describe("Testing new nix implementation", func() {
 		It("task dependency go_1_17 will have priority to bob file go_1_18", func() {
 			Expect(os.Rename("with_task_dependencies.yaml", "bob.yaml")).NotTo(HaveOccurred())
 
-			nix, err := bob.NewNix(bob.WithNixCache(&FakeCache{}))
+			nixCacheFilePath := dir + "customFile"
+
+			fileCache, err := nix2.NewFileCacheStore(nix2.WithCustomPath(nixCacheFilePath))
+			Expect(err).NotTo(HaveOccurred())
+
+			nix, err := bob.NewNix(bob.WithNixCache(fileCache))
 			Expect(err).NotTo(HaveOccurred())
 
 			b, err := bob.Bob(bob.WithDir(dir), bob.WithCachingEnabled(false), bob.WithNix(nix))
@@ -52,6 +68,11 @@ var _ = Describe("Testing new nix implementation", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(output()).To(ContainSubstring("go version go1.17.9"))
+
+			// build paths are cached in the custom file
+			data, err := os.ReadFile(nixCacheFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(data)).To(Equal(nixCacheContent))
 		})
 	})
 
