443465b2479a67c2c2d35b743c8ef218f755f5d6
Andrei Boar <zuzuleinen@users.noreply.github.com>
2022-07-08 10:13:10 +0300

    fix Empty .bobcache dir created when running build (#126)
    * fix Empty .bobcache dir created when running build
    * fix calling DefaultNix will not build .bobcache

--- bob/nix_builder_defaults.go
+++ bob/nix_builder_defaults.go
@@ -17,16 +17,12 @@ func DefaultNix() (_ *NixBuilder, err error) {
 	errz.Fatal(err)
 
 	cacheDir := filepath.Join(home, global.BobCacheNixFileName)
-	err = os.MkdirAll(filepath.Dir(global.BobCacheNixFileName), 0775)
+
+	err = os.MkdirAll(filepath.Dir(cacheDir), 0775)
 	errz.Fatal(err)
 
 	nixCache, err := nix.NewCacheStore(nix.WithPath(cacheDir))
 	errz.Fatal(err)
 
 	return NewNixBuilder(WithCache(nixCache)), nil
-}
-
-func MustDefaultNix() *NixBuilder {
-	n, _ := DefaultNix()
-	return n
 }
