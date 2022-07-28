3d26891711141f8bfa3bef4119879eca0e0c4d2d
Matthias Nösner <Equanox@users.noreply.github.com>
2022-07-28 16:03:18 +0200

    `bob clone` infere protocol by flags and from url (#144)
    * infere prefered protocol from scheme
    * select prefered protocol on cli

--- cli/cmd_clone.go
+++ cli/cmd_clone.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"errors"
 	"fmt"
+	"os"
 
 	"github.com/benchkram/bob/bob"
 	"github.com/benchkram/bob/pkg/usererror"
@@ -19,23 +20,39 @@ var CmdClone = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		failFast, err := cmd.Flags().GetBool("fail-fast")
 		errz.Fatal(err)
+		https, err := cmd.Flags().GetBool("https")
+		errz.Fatal(err)
+		ssh, err := cmd.Flags().GetBool("ssh")
+		errz.Fatal(err)
+
+		if ssh && https {
+			fmt.Printf("%s\n", aurora.Red("You can only use one of --ssh or --https"))
+			os.Exit(0)
+		}
+
+		var protocol string
+		if ssh {
+			protocol = "ssh"
+		} else if https {
+			protocol = "https"
+		}
 
 		var url string
 		if len(args) > 0 {
 			url = args[0]
 		}
-		runClone(url, failFast)
+		runClone(url, failFast, protocol)
 	},
 }
 
-func runClone(url string, failFast bool) {
+func runClone(url string, failFast bool, protocol string) {
 
 	if len(url) == 0 {
 		bob, err := bob.Bob(bob.WithRequireBobConfig())
 		errz.Fatal(err)
 
 		// Try to clone dependencies of current repo
-		err = bob.Clone(failFast)
+		err = bob.Clone(failFast, protocol)
 		errz.Fatal(err)
 
 		fmt.Printf("%s\n", aurora.Green("Cloned"))
