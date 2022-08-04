1118b82ece46d1c9628358ce9008fb81a5606a4d
Andrei Boar <andrey.boar@gmail.com>
2022-08-04 13:26:56 +0300

    remove NIX_PATH from whitelisted env

--- bob/global/env.go
+++ bob/global/env.go
@@ -1,4 +1,4 @@
 package global
 
 // EnvWhitelist keep the whitelisted env variables when hermetic mode is on
-var EnvWhitelist = []string{"HOME", "XDG_CACHE_HOME", "NIX_PATH"}
+var EnvWhitelist = []string{"HOME", "XDG_CACHE_HOME"}
