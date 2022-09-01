89bfe8370d792c15c0402dd56359a7ba8219dd9f
Andrei Boar <andrey.boar@gmail.com>
2022-09-01 11:48:04 +0300

    add warning if target path is . ./. ./.. or ..

--- bobtask/verify.go
+++ bobtask/verify.go
@@ -2,9 +2,12 @@ package bobtask
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 
+	"github.com/benchkram/bob/bobtask/target"
 	"github.com/benchkram/bob/pkg/file"
+	"github.com/benchkram/bob/pkg/usererror"
 )
 
 // Verify a bobfile before task runner.
@@ -32,7 +35,34 @@ func (t *Task) verifyBefore() (err error) {
 			}
 		}
 	}
+
+	if t.target != nil && t.target.Type() == target.Path {
+		for _, path := range t.target.Paths() {
+			if verifyTargetPath(path) {
+				return usererror.Wrap(fmt.Errorf("invalid target `%s` for task `%s`", path, t.name))
+			}
+		}
+	}
+
 	return nil
+}
+
+// verifyTargetPath reports whether the final component of path is "." or ".."
+func verifyTargetPath(path string) bool {
+	// check for end with one dot
+	if path == "." {
+		return true
+	}
+	if len(path) >= 2 && path[len(path)-1] == '.' && os.IsPathSeparator(path[len(path)-2]) {
+		return true
+	}
+
+	// check for end with two dots
+	if len(path) >= 2 && path[len(path)-1] == '.' && path[len(path)-2] == '.' {
+		return true
+	}
+
+	return false
 }
 
 func (t *Task) verifyAfter() (err error) {
