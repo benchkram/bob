c125a038c9680911fae2ffd87c2043a67937afa6
equanox <matthias@benchkram.de>
2022-11-15 22:04:51 +0100

    Documented bob.yaml

--- cli/cmd_init.go
+++ cli/cmd_init.go
@@ -26,21 +26,54 @@ var initCmd = &cobra.Command{
 	},
 }
 
-var withoutProject = `nixpkgs: https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz
+var withoutProject = `# Pin nixpkgs to get reproducable behaviour.
+nixpkgs: https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz
+
+# Declare tasks below the build keyword.
 build:
+  # build - is the default task execute when calling "bob build".
   build:
-    input: .
-    cmd: touch hello-world
-    target: hello-world
+    # Inputs of the task.
+    # If any of the inputs changes the task is rebuild.
+    # input: .
+    
+    # Command to execute
+    cmd: echo "Can we fix it? Yes we can!" 
+    
+    # Rebuild policy, defaults to [on-change]
+    rebuild: always
+    
+    # Output of a task. Can be a file, directory or docker image.
+    # Path must reside inside the scope of a repository.
+    # Is packed into an .tar.gz and stored as artifact 
+    # in the local and/or remote cache.
+    # target: run
 `
 
 var withProject = `project: %s
+
+# Pin nixpkgs to get reproducable behaviour.
 nixpkgs: https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz
+
+# Declare tasks below the build keyword.
 build:
+  # build - is the default task execute when calling "bob build".
   build:
-    input: .
-    cmd: touch hello-world
-    target: hello-world
+    # Inputs of the task.
+    # If any of the inputs changes the task is rebuild.
+    # input: .
+    
+    # Command to execute
+    cmd: echo "Can we fix it? Yes we can!" 
+    
+    # Rebuild policy, defaults to [on-change]
+    rebuild: always
+    
+    # Output of a task. Can be a file, directory or docker image.
+    # Path must reside inside the scope of a repository.
+    # Is packed into an .tar.gz and stored as artifact 
+    # in the local and/or remote cache.
+    # target: run
 `
 
 func runInit(projectName string) {
