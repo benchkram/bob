11f1b2227c74520bcf367d95af6f0132b4cc1a38
equanox <matthias@benchkram.de>
2023-01-20 11:26:40 +0100

    add more benchmarks

--- test/benchmark/bench_test.go
+++ test/benchmark/bench_test.go
@@ -6,13 +6,14 @@ import (
 	"testing"
 
 	"github.com/benchkram/bob/bob"
+	"github.com/benchkram/bob/bob/bobfile"
 	"github.com/benchkram/bob/test/setup"
 	"github.com/stretchr/testify/assert"
 )
 
 var result error
 
-func BenchmarkBob(b *testing.B) {
+func BenchmarkMultilevelBuild(b *testing.B) {
 	dir, storageDir, cleanup, err := setup.TestDirs("build-benchmark")
 	assert.Nil(b, err)
 	defer cleanup()
@@ -32,9 +33,64 @@ func BenchmarkBob(b *testing.B) {
 
 	var r error
 	for n := 0; n < b.N; n++ {
-		// always record the result of Fib to prevent
-		// the compiler eliminating the function call.
 		r = bobInstance.Build(ctx, bob.BuildAllTargetName)
+
+	}
+	// always store the result to a package level variable
+	// so the compiler cannot eliminate the Benchmark itself.
+	result = r
+
+	b.ReportAllocs()
+}
+
+var resultAggregate *bobfile.Bobfile
+
+func BenchmarkAggregate(b *testing.B) {
+	dir, storageDir, cleanup, err := setup.TestDirs("build-benchmark")
+	assert.Nil(b, err)
+	defer cleanup()
+
+	err = os.Chdir(dir)
+	assert.Nil(b, err)
+
+	bobInstance, err := bob.BobWithBaseStoreDir(storageDir, bob.WithDir(dir))
+	assert.Nil(b, err)
+
+	err = bob.CreatePlayground(bob.PlaygroundOptions{Dir: dir})
+	assert.Nil(b, err)
+
+	var r *bobfile.Bobfile
+	for n := 0; n < b.N; n++ {
+		r, _ = bobInstance.Aggregate()
+
+	}
+	// always store the result to a package level variable
+	// so the compiler cannot eliminate the Benchmark itself.
+	resultAggregate = r
+
+	b.ReportAllocs()
+}
+
+func BenchmarkFilterInputs(b *testing.B) {
+	dir, storageDir, cleanup, err := setup.TestDirs("build-benchmark")
+	assert.Nil(b, err)
+	defer cleanup()
+
+	err = os.Chdir(dir)
+	assert.Nil(b, err)
+
+	bobInstance, err := bob.BobWithBaseStoreDir(storageDir, bob.WithDir(dir))
+	assert.Nil(b, err)
+
+	err = bob.CreatePlayground(bob.PlaygroundOptions{Dir: dir})
+	assert.Nil(b, err)
+
+	aggregate, err := bobInstance.Aggregate()
+	assert.Nil(b, err)
+
+	var r error
+	for n := 0; n < b.N; n++ {
+		r = aggregate.BTasks.FilterInputs()
 
 	}
 	// always store the result to a package level variable
