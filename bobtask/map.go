28b2652e4065a8692bf8c0c9308c8e2faa695b82
Tasos Papalyras <tasos@rdnt.dev>
2023-04-10 13:38:09 +0300

    Fix child bobfile target ignore

--- bobtask/map.go
+++ bobtask/map.go
@@ -189,7 +189,8 @@ func (tm Map) CollectNixDependenciesForTasks(whitelist []string) ([]nix.Dependen
 
 // IgnoreChildTargets fills the `InputAdditionalIgnores` field of each task
 // with the targets of each tasks children.
-func (tm Map) IgnoreChildTargets() (err error) {
+// dir should be the root of the aggregate, and it is used to ignore resolved targets relative to this project
+func (tm Map) IgnoreChildTargets(dir string) (err error) {
 	defer errz.Recover(&err)
 	for name, umbrellaTask := range tm {
 
@@ -222,7 +223,7 @@ func (tm Map) IgnoreChildTargets() (err error) {
 						//
 						//     third-level    fouth-level           second-level/third-level/fourth-level/target
 
-						relP, err := filepath.Rel(umbrellaTask.Dir(), p)
+						relP, err := filepath.Rel(dir, p)
 						if err != nil {
 							return err
 						}
