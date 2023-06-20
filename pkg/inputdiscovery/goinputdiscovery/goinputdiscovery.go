832a0c8c8cfa840729c701e5a593c2b8d209dcbb
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2023-06-20 08:15:12 +0200

    Add support for 'replace' and refactor code into goInputDiscovery

--- pkg/inputdiscovery/goinputdiscovery/goinputdiscovery.go
+++ pkg/inputdiscovery/goinputdiscovery/goinputdiscovery.go
@@ -16,12 +16,16 @@ var Keyword = "go"
 
 type goInputDiscovery struct {
 	projectDir string
+
+	closed map[string]bool
 }
 
 type Option func(discovery *goInputDiscovery)
 
 func New(options ...Option) inputdiscovery.InputDiscovery {
 	id := &goInputDiscovery{}
+
+	id.closed = make(map[string]bool)
 	for _, opt := range options {
 		opt(id)
 	}
@@ -73,9 +77,7 @@ func (id *goInputDiscovery) DiscoverInputs(packagePathAbs string) (_ []string, e
 
 	prefix := packageName + "/"
 
-	dr := newDepResolver()
-
-	paths := sliceutil.Unique(dr.localDependencies(pkg.Imports, prefix))
+	paths := sliceutil.Unique(id.localDependencies(pkg.Imports, prefix))
 
 	var resultAbs []string
 	for _, p := range paths {
@@ -113,4 +115,43 @@ func (id *goInputDiscovery) DiscoverInputs(packagePathAbs string) (_ []string, e
 	}
 
 	return resultRel, nil
+}
+
+// localDependencies is a recursive function that resolves the tree of imports provided by x/tools/packages
+// it returns only packages with an ID-prefix same as the provided prefix
+// it uses the closed variable to keep track of which packages were already handled to prevent an endless loop
+func (id *goInputDiscovery) localDependencies(imports map[string]*packages.Package, prefix string) []string {
+	var deps []string
+	for _, pkg := range imports {
+		// if the package is a local package add its whole dir
+		if strings.HasPrefix(pkg.ID, prefix) {
+			slug := strings.TrimPrefix(pkg.ID, prefix)
+			slugParts := strings.Split(slug, "/")
+			if len(slugParts) > 0 {
+				deps = append(deps, slugParts[0]+"/")
+			}
+
+		} else if pkg.Module != nil && pkg.Module.Replace != nil && strings.HasPrefix(pkg.Module.Replace.Path, "./") {
+			// go.mod allows to replace modules: https://go.dev/ref/mod#go-mod-file-replace
+			// if the replacement is a local path starting with './' we add it to the local dependencies
+			// go.mod also allows '../' paths, but we require that all content is inside the bob folder, so we ignore it
+			deps = append(deps, strings.TrimPrefix(pkg.Module.Replace.Path, "./")+"/")
+		} else if pkg.Module != nil && strings.HasPrefix(pkg.Module.Dir, id.projectDir) {
+			// if go.work is used to extend the workspace we need to find the packages
+			deps = append(deps, strings.TrimPrefix(pkg.Module.Dir, id.projectDir+"/"))
+		}
+
+		open := make(map[string]*packages.Package)
+		for pkgId, secondLevelPkg := range pkg.Imports {
+			if !id.closed[pkgId] {
+				open[pkgId] = secondLevelPkg
+				id.closed[pkgId] = true
+			}
+		}
+		if len(open) > 0 {
+			newDeps := id.localDependencies(open, prefix)
+			deps = append(deps, newDeps...)
+		}
+	}
+	return deps
 }
